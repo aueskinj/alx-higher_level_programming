8a61217656cb583cfb60817d84bd38f957eca071
aueskinj <aueskinj@gmail.com>
2022-12-02 10:34:21 +0300

    reverted to old one

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,3 +1,7 @@
-#!/usr/bin/python3
-print(u"\x41\x42\x43\x44\x45\x46\x47\x48\x49\x4A\x4B\x4C\x4D\x4E\x4F", end='')
-print(u"\x50\x51\x52\x53\x54\x55\x56\x57\x58\x59\x5A")
+#!/usr/bin/python3 
+def uppercaseAlphabets():
+    for c in range(65, 91):
+         print(chr(c), end = " ");
+    print("");
+
+uppercaseAlphabets();
