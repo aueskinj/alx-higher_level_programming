0419793cd9382a3dc3321533c5625eb6169cab5b
aueskinj <71489113+aueskinj@users.noreply.github.com>
2022-12-05 07:41:26 +0300

    all

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
+# Author -Godswill Kalu
 
 def pow(a, b):
-    """Return a to the power of b."""
     return (a ** b)
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+# Author - Godswill Kalu
 
 def uppercase(str):
     """Print a string in uppercase."""
