819a2d698f5809c07902e27323e7eaffc81f0795
kimuhuNjuguna <aueskinj@gmail.com>
2022-12-08 06:45:40 +0000

    please new betty

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -6,7 +6,7 @@ def square_matrix_simple(matrix=[]):
     wordA function that computes the square
     value of all intergers of a matrix.
     '''
-    new_matrix=[]
+    new_matrix = []
     for col in matrix:
         result = list(map(lambda x: x**2, col))
         new_matrix.append(result)
