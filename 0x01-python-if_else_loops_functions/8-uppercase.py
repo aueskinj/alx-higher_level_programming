150b99aff0d966ad3a5b510b8b2e8d0e1db2b7ed
kimuhuNjuguna <aueskinj@gmail.com>
2025-02-23 11:52:36 +0000

    task 9

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def uppercase(c):
+    """Converts lowercase to uppercase"""
+    newc = ''
+    for i in c:
+        if 97 <= ord(i) <= 122:
+            letter = ord(i) -32
+            newc = newc + chr(letter)
+        else:
+            newc = newc + i
+        
+    else:
+        return newc
