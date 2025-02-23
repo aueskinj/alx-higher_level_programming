88c732e75188a40784eacf8a05c318e352474d28
kimuhuNjuguna <aueskinj@gmail.com>
2025-02-23 10:07:23 +0000

    fixed by adding the shebang

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,8 +1,10 @@
+#!/usr/bin/env python3
 def islower(letter):
     """Scans each character in text.
 Converts only lowercase letters (a-z) to uppercase (A-Z).
 Leaves non-letter characters (digits, spaces, punctuation, etc.) unchanged.
 Returns a new string."""
+
     for i in letter:
         if 97 <= ord(i) <=122:
             return True
