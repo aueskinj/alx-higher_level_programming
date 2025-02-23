98fdba84303371499f4c8d9ed2f04e95e92e7531
kimuhuNjuguna <aueskinj@gmail.com>
2025-02-23 10:05:32 +0000

    task 8

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,8 @@
+def islower(letter):
+    """Scans each character in text.
+Converts only lowercase letters (a-z) to uppercase (A-Z).
+Leaves non-letter characters (digits, spaces, punctuation, etc.) unchanged.
+Returns a new string."""
+    for i in letter:
+        if 97 <= ord(i) <=122:
+            return True
