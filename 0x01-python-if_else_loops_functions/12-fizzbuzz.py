6f3e17b27d38a3f8555de097722689030b6092b1
kimuhuNjuguna <aueskinj@gmail.com>
2025-02-23 13:01:23 +0000

    added multiples of both 5 and 3

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,10 +3,13 @@
 def fizzbuzz():
     '''showing multiples of three(fizz) and five(buzz)'''
     for i in range(1, 101):
-        if i % 3 == 0:
-            print('Fizz ', end="")
+        if i % 5  == 0 and i % 3 == 0:
+            print('FizzBuzz ', end="")
+            continue
+        elif i % 3 == 0:
+            print('Fizz ', end='')
         elif i % 5 == 0:
             print('Buzz ', end='')
+
         else:
             print('{} '.format(i), end='')
- 
