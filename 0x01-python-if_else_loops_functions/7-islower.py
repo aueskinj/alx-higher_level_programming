71eea29b8ca21bb0711cd4f583b31dadf1608be5
kimuhuNjuguna <aueskinj@gmail.com>
2025-02-23 10:18:37 +0000

    removed the unnecessary for loop

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
 
-def islower(letter):
-    """Scans each character in text.
-Converts only lowercase letters (a-z) to uppercase (A-Z).
-Leaves non-letter characters (digits, spaces, punctuation, etc.) unchanged.
-Returns a new string."""
-
-    for i in letter:
-        if 97 <= ord(i) <= 122:
-            return True
-        else:
-            return False
+def islower(c):
+    """Checks if a character is lowercase."""
+    if 97 <= ord(c) <= 122:
+        return True 
+    else:
+        return False
