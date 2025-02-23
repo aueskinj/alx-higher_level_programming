113d3934895e4846cdb4107cd66a656b5d0d2f69
kimuhuNjuguna <aueskinj@gmail.com>
2025-02-23 09:49:38 +0000

    oops, used f" and not .format again. Pushing the fix

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -11,5 +11,6 @@ You can only use no more than 2 loops in your code
 You are not allowed to store numbers or strings in a variable
 You are not allowed to import any module"""
 
-print(", ".join(f"{d1}{d2}" for d1 in range(10) for d2 in range(d1 + 1, 10)), end="")
+print(", ".join("{}{}".format(d1, d2) for d1 in range(10) for d2 in range(d1 + 1, 10)))
+
           
