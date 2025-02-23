eeb6fd71c67b7013223038f4a9db382a8bf99fac
kimuhuNjuguna <aueskinj@gmail.com>
2025-02-23 09:29:30 +0000

    used ,format insted of f"

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,7 +3,7 @@
 for i in range(0, 10):
     for k in range(0, 10):
         if i == 9 and k == 9:  # Last number (99) should not have a comma
-            print(f"{i}{k}")
+            print("{}{}".format(i,k))
         else:
-            print(f"{i}{k}, ", end="")
+            print("{}{}, ".format(i,k), end="")
           
