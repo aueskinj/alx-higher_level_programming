29edd8feee64e3d1b221ff08dd708d97c9ef67a8
kimuhuNjuguna <aueskinj@gmail.com>
2025-02-23 09:50:32 +0000

    pep8 compliant code

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,16 +1,7 @@
 #!/usr/bin/python3
 
-"""Numbers must be separated by ,, followed by a space
-The two digits must be different
-01 and 10 are considered the same combination of the two digits 0 and 1
-Print only the smallest combination of two digits
-Numbers should be printed in ascending order, with two digits
-The last number should be followed by a new line
-You can only use no more than 3 print functions with string format
-You can only use no more than 2 loops in your code
-You are not allowed to store numbers or strings in a variable
-You are not allowed to import any module"""
+print(", ".join(
+    "{}{}".format(d1, d2)
+    for d1 in range(10) for d2 in range(d1 + 1, 10)
+))
 
-print(", ".join("{}{}".format(d1, d2) for d1 in range(10) for d2 in range(d1 + 1, 10)))
-
-          
