6b19d233799ac83b56dc2f7f3116fde4753faf2e
kimuhuNjuguna <aueskinj@gmail.com>
2025-02-23 11:58:17 +0000

    fixed for pep8 compliance

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -5,7 +5,7 @@ def uppercase(c):
     newc = ''
     for i in c:
         if 97 <= ord(i) <= 122:
-            letter = ord(i) -32
+            letter = ord(i) - 32
             newc = newc + chr(letter)
         else:
             newc = newc + i
