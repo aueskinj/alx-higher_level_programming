da6bdf7816fde3848e4d2b975375377f0916de1b
kimuhuNjuguna <aueskinj@gmail.com>
2025-02-25 06:51:34 +0000

    pythonic code achieved

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,14 +3,11 @@
 import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
+    arg_count = len(sys.argv) - 1
+    if arg_count == 0:
         print("0 arguments.")
-
-    elif(len(sys.argv) == 2):
-        print("{} argument:".format(len(sys.argv[1:])))
-        print("1 : {}".format(sys.argv[1]))
-    elif(len(sys.argv) > 2):
-        print("{} arguments:".format(len(sys.argv)))
-        for index, arg in enumerate(sys.argv[1:], start = 1):
-            print("{} : {}".format(index, arg))  
+    else:
+        print(f"{arg_count} argument{'s' if arg_count > 1 else ''}:")
+        for index, arg in enumerate(sys.argv[1:], start=1):
+            print(f"{index}: {arg}")  
     
