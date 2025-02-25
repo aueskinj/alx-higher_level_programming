7ccb7dc4e1a541e6296cde5f7fa00b5eeda54a4f
kimuhuNjuguna <aueskinj@gmail.com>
2025-02-25 11:40:24 +0000

    complex approach

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import dis
+import marshal
+import sys
+
+def extract_pyc_names(pyc_file):
+    with open(pyc_file, "rb") as f:
+        f.read(16)  # Skip the header (magic number, timestamp, etc.)
+        code_obj = marshal.load(f)  # Load compiled bytecode
+
+    for instr in dis.get_instructions(code_obj):
+        if instr.opname == "STORE_NAME" and not instr.argval.startswith("__"):
+            print(instr.argval)
+
+if __name__ == "__main__":
+    extract_pyc_names('hidden_4.pyc')
