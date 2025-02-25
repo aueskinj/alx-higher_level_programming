b6707bd44d89d516fb9e89e952f9ac3ca611fd0d
kimuhuNjuguna <aueskinj@gmail.com>
2025-02-25 06:42:36 +0000

    this is done

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("0 arguments.")
+        sys.exit(1)
+
+    elif(len(sys.argv) == 2):
+        print("{} argument:".format(len(sys.argv[1:])))
+        print("1 : {}".format(sys.argv[1]))
+    elif(len(sys.argv) > 2):
+        print("{} arguments:".format(len(sys.argv)))
+        for index, arg in enumerate(sys.argv[1:], start = 1):
+            print("{} : {}".format(index, arg))  
+
+    
