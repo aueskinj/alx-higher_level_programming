80bf1651a06e43a34e80a10a7f517cf79ed97570
kimuhuNjuguna <aueskinj@gmail.com>
2025-03-07 12:01:43 +0000

    task seven fix. Modified so each dict item is printed on new line

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 def print_sorted_dictionary(a_dictionary):
-    # a_dictionary = {key: a_dictionary[key] for key in sorted(a_dictionary)}
-    for key, value in sorted(a_dictionary), a_dictionary.values:
-        print("{}:{}".format(key, value))
+    keys = list(a_dictionary)
+    for i in sorted(keys):
+        print("{}: {}".format(i, a_dictionary[i]) )
 
 if __name__ == "__main__":
     print_sorted_dictionary = __import__('6-print_sorted_dictionary').print_sorted_dictionary
