061c74a73b55feb5b06985918392a9eb76737587
kimuhuNjuguna <aueskinj@gmail.com>
2025-03-13 11:38:38 +0000

    thtask two

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+def safe_print_integer(value):
+    try:
+        print("{:d}".format(value))  # Prints the integer
+        return True
+    except (ValueError, TypeError):
+        return False
+
+if __name__ == '__main__':
+    safe_print_integer = __import__('1-safe_print_integer').safe_print_integer
+
+    value = 89
+    has_been_print = safe_print_integer(value)
+    if not has_been_print:
+        print("{} is not an integer".format(value))
+
+    value = -89
+    has_been_print = safe_print_integer(value)
+    if not has_been_print:
+        print("{} is not an integer".format(value))
+
+    value = "School"
+    has_been_print = safe_print_integer(value)
+    if not has_been_print:
+        print("{} is not an integer".format(value))
+"""
+Prototype: def safe_print_integer(value):
+value can be any type (integer, string, etc.)
+The integer should be printed followed by a new line
+Returns True if value has been correctly printed (it means the value is an integer)
+Otherwise, returns False
+You have to use try: / except:
+You have to use "{:d}".format() to print as integer
+You are not allowed to import any module
+You are not allowed to use type()
+"""
