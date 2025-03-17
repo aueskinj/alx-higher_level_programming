afed8564b0f106c78cb530844b633c3ce8aa8878
kimuhuNjuguna <aueskinj@gmail.com>
2025-03-17 13:33:04 +0000

    task five

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+
+"""Prototype: def list_division(my_list_1, my_list_2, list_length):
+my_list_1 and my_list_2 can contain any type (integer, string, etc.)
+list_length can be bigger than the length of both lists
+Returns a new list (length = list_length) with all divisions
+If 2 elements can’t be divided, the division result should be equal to 0
+If an element is not an integer or float:
+print: wrong type
+If the division can’t be done (/0):
+print: division by 0
+If my_list_1 or my_list_2 is too short
+print: out of range
+You have to use try: / except: / finally:
+You are not allowed to import any module"""
+
+def list_division(my_list_1, my_list_2, list_length):
+    new_list = []
+    for i in range(list_length):
+        try:
+            new_list.append(my_list_1[i]/my_list_2[i])
+        except TypeError:
+            print("wrong type")
+            new_list.append(0)
+        except ZeroDivisionError:
+            print("division by 0")
+            new_list.append(0)
+        except IndexError:
+            print("out of range")
+            new_list.append(0)
+    return (new_list)
+
+
+
+if __name__ == "__main__":
+    my_l_1 = [10, 8, 4]
+    my_l_2 = [2, 4, 4]
+    result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
+    print(result)
+
+    print("--")
+
+    my_l_1 = [10, 8, 4, 4]
+    my_l_2 = [2, 0, "H", 2, 7]
+    result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
+    print(result)
--- 0x05-python-exceptions/try.py
+++ 0x05-python-exceptions/try.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-my_list = [1, 2, 3, 4, 5, "dg"]
-for i in my_list:
+my_list = [1, 2, 3, 4, 5,]
+list2 = [5, 4, 3, 2, 1]
+for i,k in zip(my_list, list2):
     try:
-        int(i)
-        print(i)
+        print(i, k)
     except (ValueError, TypeError):
         continue
