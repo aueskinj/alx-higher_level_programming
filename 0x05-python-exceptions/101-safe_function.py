122847954d2c94c88cdb6e64d3a2a7b5ea879806
kimuhuNjuguna <aueskinj@gmail.com>
2025-03-25 13:48:43 +0000

    done for the day!

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,31 @@
+#!/usr/bin/bash
+
+def safe_function(fct, *args):
+    import sys
+    try:
+        return (fct(*args)) 
+    except Exception as e:
+        print("Exception {} ".format(e), file=sys.stderr)
+        return False
+
+if __name__ == "__main__":
+    def my_div(a, b):
+        return a / b
+
+    result = safe_function(my_div, 10, 2)
+    print("result of my_div: {}".format(result))
+
+    result = safe_function(my_div, 10, 0)
+    print("result of my_div: {}".format(result))
+
+
+    def print_list(my_list, len):
+        i = 0
+        while i < len:
+            print(my_list[i])
+            i += 1
+        return len
+
+    result = safe_function(print_list, [1, 2, 3, 4], 10)
+    print("result of print_list: {}".format(result))
+
