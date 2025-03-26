8ebc9da0bcfbc31cf35da1656877ef79967b3113
kimuhuNjuguna <aueskinj@gmail.com>
2025-03-26 09:33:26 +0000

    removed extra line at end of code

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-def mystery_function(a, b):
+def magic_calculation(a, b):
     result = 0
     for i in range(1, 3):
         try:
             if i > a:
-                raise Exception("Too far")
-            result += (a ** b) / i
-        except:
-            result = a + b # This part is redundant in Python as `break` is inside `except`
+                raise Exception('Too far')
+            result += a ** b / i
+        except Exception:
+            result = b + a
             break
     return result
-
