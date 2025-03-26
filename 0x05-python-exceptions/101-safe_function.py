3863c1d368df50136484fbdef154c8352c0c97fa
kimuhuNjuguna <aueskinj@gmail.com>
2025-03-26 08:16:29 +0000

    task nine

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/usr/bin/python3
 
 def safe_function(fct, *args):
     import sys
@@ -6,7 +6,7 @@ def safe_function(fct, *args):
         return (fct(*args)) 
     except Exception as e:
         print("Exception {} ".format(e), file=sys.stderr)
-        return False
+        return None
 
 if __name__ == "__main__":
     def my_div(a, b):
