d3420a4d60bf01f97cd14171c0a7eea7494dd32a
kimuhuNjuguna <aueskinj@gmail.com>
2025-05-05 13:51:50 +0000

    last task

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,52 @@
+class Node:
+    def __init__(self, data, next_node=None):
+        """Initialise a new node instance"""
+        self.__data = data
+        self.__next_node = next_node
+
+    @property
+    def data(self):
+        return self.__data
+    
+    @data.setter
+    def data(self, value):
+        if not isinstance(value, int):
+            raise TypeError("data must be an integer")
+        self.__data = value
+    
+    @property
+    def next_node(self):
+        return self.__next_node
+    
+    @next_node.setter
+    def next_node(self, value):
+        if not isinstance(value, Node) and value is not None:
+            raise TypeError("next_node must be a Node object")
+        self.__next_node = value
+    
+class SinglyLinkedList:
+    def __init__(self):
+        """initialise a singly linked list"""
+        self.__head = None
+    
+    def sorted_insert(self, value):
+        """inserts new node into the list in increasing order"""
+        new_node = Node(value)
+        if self.__head is None or self.__head.data > value:
+            new_node.next_node = self.__head
+            self.__head = new_node
+        else:
+            current = self.__head
+            while current.next_node and current.next_node.data < value:
+                current = current.next_node
+            new_node.next_node = current.next_node
+            current.next_node = new_node
+            
+    def __str__(self):
+        """returns the string representation of the list"""
+        current = self.__head
+        result = []
+        while current is not None:
+            result.append(str(current.data))
+            current = current.next_node
+        return "\n".join(result)
